ae25cbd9809fdbe1f1071f4d1828b06fe7adcb1a
chenkangaroo <chends0314@gmail.com>
2019-06-09 16:34:33 +0800

    feat: add css support

--- src/search.js
+++ src/search.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './search.css'
 
 
 class Search extends React.Component {
   render() {
     return (
-      <div>Search</div>
+      <div className='search-text'>Search</div>
     )
   }
 } 
--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   }
