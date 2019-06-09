86ad7ebbd703b804fdb843de09f78513cbb17e1d
chenkangaroo <chends0314@gmail.com>
2019-06-09 16:15:56 +0800

    feat: add react react-preset-env

--- src/search.js
+++ src/search.js
@@ -1,1 +1,14 @@
-document.write('search page')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div>Search</div>
+    )
+  }
+} 
+
+
+ReactDOM.render(<Search />, document.getElementById('root'))
