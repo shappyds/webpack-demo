da28913c6d3bc1c8aee618f52d6ca3c1745d982a
chenkangaroo <chends0314@gmail.com>
2019-06-10 01:46:52 +0800

    feat: add hash for cache

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
@@ -7,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'), // must be absolute path
-    filename: '[name].js'
+    filename: '[name]_[chunkhash:8].js'
   },
   // development 会设置process.env.NODE_ENV的值为development,开启NamedChunksPlugin和NamedModulesPlugin
   // productiont 会设置process.env.NODE_ENV的值为production, 开启一系列插件
@@ -22,10 +24,16 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader'
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css'
+    }),
+    new webpack.HashedModuleIdsPlugin()
+  ]
 } 
