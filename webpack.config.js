7b5874b660d6459fbf542aa7370b59bc01a5c056
chenkangaroo <chends0314@gmail.com>
2019-06-09 15:57:20 +0800

    feat: init webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'), // must be absolute path
+    filename: '[name].js'
+  },
+  // development 会设置process.env.NODE_ENV的值为development,开启NamedChunksPlugin和NamedModulesPlugin
+  // productiont 会设置process.env.NODE_ENV的值为production, 开启一系列插件
+  // none 不开启任何优化选项
+  mode: 'production'
+} 
