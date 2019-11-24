424dfca6fd81309ac06fe745fd212561d5484f0b
Todd Chen <toddchen@nd.com.cn>
2019-11-24 16:35:01 +0800

    feat: mutli page setup

--- src/search/index.js
+++ src/search/index.js
@@ -1,11 +1,9 @@
-import { hello } from './hello'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
 
 const Index = () => {
   return (
-    <div className="root-text">{hello()}</div>
+    <div>{'search'}</div>
   )
 }
 
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,13 +1,53 @@
 'use strict'
 
+const glob = require('glob')
 const path = require('path')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+
+const setMPA = () => {
+  const entry = {}
+  const htmlWebpackPlugin = []
+  const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'))
+  
+  Object.keys(entryFiles)
+    .map(index => {
+      const entryFile = entryFiles[index]
+      const match = entryFile.match(/src\/(.*)\/index.js/)
+      const pageName = match && match[1]
+      
+      entry[pageName] = entryFile
+      htmlWebpackPlugin.push(
+        new HtmlWebpackPlugin({
+          template: path.join(__dirname, `src/${pageName}/index.html`),
+          filename: `${pageName}.html`,
+          inject: true,
+          chunks: [pageName],
+          minify: {
+            html5: true,
+            collapseWhitespace: true,
+            preserveLineBreaks: false,
+            minifyCSS: true,
+            minifyJS: true,
+            removeComments: false
+          }
+        }),
+      )
+    })
+
+  return {
+    entry,
+    htmlWebpackPlugin
+  }
+}
+
+const { entry, htmlWebpackPlugin } = setMPA()
+
 module.exports = {
-  entry: './src/index.js',
+  entry,
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name]_[chunkhash:8].js'
@@ -40,19 +80,7 @@ module.exports = {
       assetNameRegExp: /\.css$/g,
       cssProcessor: require('cssnano')
     }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/template/index.html'),
-      filename: 'index.html',
-      inject: true,
-      minify: {
-        html5: true,
-        collapseWhitespace: true,
-        preserveLineBreaks: false,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false
-      }
-    }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    ...htmlWebpackPlugin
   ],
 }
